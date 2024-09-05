24e1e7c078a0f4ab1b2567f6036bb6762b18338c
Antonyxit <carlosmorga45@gmail.com>
2024-09-05 10:38:49 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -15,9 +15,9 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   const oi = `${tradutor.texto1[0]} ${pesan}`;
   let teks = `${tradutor.texto1[1]}  ${oi}\n\n${tradutor.texto1[2]}\n`;
   for (const mem of participants) {
-    teks += `┣➥ ⚡ @${mem.id.split('@')[0]}\n`;
+    teks += `┣➥ 🤝😂 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐓𝐇𝐄𝐌𝐎𝐍𝐀𝐑𝐂𝐇 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝐂𝐄𝐋𝐎𝐒𝐀 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
