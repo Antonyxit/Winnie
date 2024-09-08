3e0afaab67569d112806c75441c7797925f42abf
Antonyxit <carlosmorga45@gmail.com>
2024-09-08 12:27:39 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,19 +12,19 @@ global.confirmCode = ""
 
 global.owner = [
   ['5214461281232', '👑 danny - Creador 👑', true],
-  ['5217443817277', '𝐏𝐑𝐎 𝐁𝐎𝐓 - Collaborator 2 💫', true],
-  ['5217443817277']
+  ['5214794070450', '𝐏𝐑𝐎 𝐁𝐎𝐓 - Collaborator 2 💫', true],
+  ['5214794070450']
 ];
 
-global.suittag = ['5217443817277'];
-global.prems = ['5217443817277'];
+global.suittag = ['5214794070450'];
+global.prems = ['5214794070450'];
 
 global.packname = '𝐒𝐓𝐈𝐂𝐊𝐄𝐑';
-global.author = '𝐀𝐋𝐄𝐗 𝐁𝐎𝐓';
-global.wm = '𝐀𝐋𝐄𝐗 𝐁𝐎𝐓';
-global.titulowm = '𝐀𝐋𝐄𝐗 𝐁𝐎𝐓';
-global.titulowm2 = `𝐀𝐋𝐄𝐗 𝐁𝐎𝐓`
-global.igfg = '𝐀𝐋𝐄𝐗 𝐁𝐎𝐓';
+global.author = '𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓';
+global.wm = '𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓';
+global.titulowm = '𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓';
+global.titulowm2 = `𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓`
+global.igfg = '𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓';
 global.wait = '*_[ ⏳ ] Cargando..._*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -49,16 +49,16 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\n𝐀𝐋𝐄𝐗 𝐁𝐎𝐓`;
-global.gt = '𝐀𝐋𝐄𝐗 𝐁𝐎𝐓';
-global.mysticbot = '𝐀𝐋𝐄𝐗 𝐁𝐎𝐓';
+global.wm2 = `${dia} ${fecha}\n𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓`;
+global.gt = '𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓';
+global.mysticbot = '𝐒𝐓𝐈𝐂𝐇 𝐁𝐎𝐓';
 global.channel = '';
 global.md = '';
 global.mysticbot = '';
 global.waitt = '*_[ ⏳ ] Cargando..._*';
 global.waittt = '*_[ ⏳ ] Cargando..._*';
 global.waitttt = '*_[ ⏳ ] Cargando..._*';
-global.nomorown = '5217443817277';
+global.nomorown = '5214794070450';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
